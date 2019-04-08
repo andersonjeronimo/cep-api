8596cdaddfaece18344d3531f0eb840ba1c14b1a
Anderson Luiz F Jeronimo <anderson.luiz.sjc@gmail.com>
2019-04-08 07:36:44 -0300

    conexão com Mongodb desabilitada

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3001,
-  mongoose = require("mongoose"),
-  Message = require("./api/models/addressModel"),
+  port = process.env.PORT || 3001;
+  /* mongoose = require("mongoose"),
+  Message = require("./api/models/addressModel"), */
   bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/addressdb");
+/* mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/addressdb"); */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var routes = require("./api/routes/addressRoutes");
